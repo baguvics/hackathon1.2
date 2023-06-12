4f3ea4e840e639bd5b5de690cb3e59304e299d59
baguvics <kurbanov_meka@mail.ru>
2023-06-12 23:08:31 +0700

    +model db

--- back_end/back_end_app/models.py
+++ back_end/back_end_app/models.py
@@ -1,15 +1,16 @@
 from django.db import models
-from django.contrib.auth.models import AbstractUser, User
-
+from django.contrib.auth.models import User
 
 
 class Article(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE, blank=True, null=True)
+    name_video = models.CharField(max_length=255)
+    title_article = models.CharField(max_length=255)
     video_url = models.CharField(max_length=255)
     content = models.TextField()
+    date_created = models.DateTimeField(auto_now_add=True)
+    images = models.ImageField(upload_to='images/')
+    keywords = models.CharField(max_length=255, blank=True, null=True)
 
-
-# модель для хранения информации о подтверждении email
-class EmailConfirmation(models.Model):                          
-    user = models.OneToOneField(User, on_delete=models.CASCADE)
-    token = models.CharField(max_length=255)
-    confirmed = models.BooleanField(default=False)
+    def str(self):
+        return self.title_article
